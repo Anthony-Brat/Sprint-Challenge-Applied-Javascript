033f358431917790873da9758c998062b220d174
unknown <tonybrat74@gmail.com>
2020-04-10 10:23:21 -0700

    hdrdone

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@
 // Write a function that returns the markup you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div>
@@ -12,13 +12,38 @@
 // and append it to the DOM inside the div.header-container
 
 function Header() {
-
-const headerComponent = document.createElement('div');
+// create elements
+const hdrDiv = document.createElement('div');
 const hdrDate = document.createElement('span');
 const title = document.createElement('h1');
-const hdrTemp =document.createElement('span');
+const hdrTemp = document.createElement('span');
+
+
+// nest
+hdrDiv.append(hdrDate);
+hdrDiv.append(title);
+hdrDiv.append(hdrTemp);
+
+
+// create classes
+hdrDiv.classList.add('header')
+hdrDate.classList.add('date')
+hdrTemp.classList.add('temp')
+
+
+// add content
+hdrDate.textContent = 'MARCH 28, 2019';
+title.textContent = 'Lambda Times';
+hdrTemp.textContent = '98°';
 
 
 
 
+return hdrDiv
 }
+const headerContainer = document.querySelector('.header-container');
+//         console.log('.header-container') to see if header-container div was selected
+
+const menu = new Header();
+
+headerContainer.append(menu);
